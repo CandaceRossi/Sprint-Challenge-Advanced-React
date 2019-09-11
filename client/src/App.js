4caabecc27314ca4f1467389dd9d946d8e14aab0
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-09-11 09:34:34 -0400

    editing class component

--- client/src/App.js
+++ client/src/App.js
@@ -6,53 +6,80 @@ import ToggleButton from "./components/ToggleButton";
 import PlayerList from "./components/PlayerList";
 import PlayerForm from "./components/PlayerForm";
 
-const App = () => {
-  const [playerData, setPlayerData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/players/")
-      .then(response => setPlayerData(response.data));
-  }, []);
-  console.log("info", playerData);
-  const handleSubmit = (event, newPlayer) => {
-    event.preventDefault();
-    let newPlayerData = {
-      playerData: newPlayer,
-      id: Date.now(),
-      completed: false
+class App extends React.Component {
+  constructor() {
+    super();
+    this.setState = {
+      playerData: []
     };
-    const updatedPlayerList = [...playerData, newPlayerData];
-    setPlayerData({
-      playerData: updatedPlayerList
-    });
-    localStorage.setItem("PlayerList", JSON.stringify(updatedPlayerList));
-  };
-  const toggleCompleted = id => {
-    const newPlayerById = playerData.map(playerData => {
-      return playerData.id === id
-        ? { ...playerData, completed: !playerData.completed }
-        : playerData;
-    });
-    setPlayerData({ playerData: newPlayerById });
-    localStorage.setItem("PlayerList", JSON.stringify(newPlayerById));
-  };
-  const clearCompleted = () => {
-    const completed = this.state.playerData.filter(playerData => {
-      return playerData.completed === false;
-    });
-    setPlayerData({
-      playerData: completed
-    });
-    localStorage.setItem("playerData", JSON.stringify(completed));
-  };
-  return (
-    <div className="App">
-      <ToggleButton />
-      <PlayerForm handleSubmit={handleSubmit} clearCompleted={clearCompleted} />
-      <PlayerList playerData={playerData} toggleCompleted={toggleCompleted} />
-    </div>
-  );
-};
+  }
+
+  componentDidMount = () => {
+    
+      axios
+        .get("http://localhost:5000/api/players/")
+        .then(response => this.setState({playerData: response.data})
+        .catch(err => console.log(err))
+  }
+  const handleSubmit = (event, newPlayer) => {
+      event.preventDefault();
+      let newPlayerData = {
+        playerData: newPlayer,
+        id: Date.now(),
+        completed: false
+      };
+    }
+      const updatedPlayerList = [...this.state.playerData, newPlayerData];
+      this.setState({
+        playerData: updatedPlayerList
+      });
+      localStorage.setItem("PlayerList", JSON.stringify(updatedPlayerList));
+    
+    const toggleCompleted = id => {
+      const newPlayerById = this.state.playerData.map(playerData => {
+        return playerData.id === id
+          ? { ...playerData, completed: !playerData.completed }
+          : playerData;
+      });
+      this.setState({ playerData: newPlayerById });
+      localStorage.setItem("PlayerList", JSON.stringify(newPlayerById));
+    };
+    const clearCompleted = () => {
+      const completed = this.state.playerData.filter(playerData => {
+        return playerData.completed === false;
+      });
+      this.setState({
+        playerData: completed
+      });
+      localStorage.setItem("playerData", JSON.stringify(completed));
+    };
+
+  render() {
+    return (
+      <div className="App">
+        <ToggleButton />
+        <PlayerForm
+          handleSubmit={this.handleSubmit}
+          clearCompleted={this.clearCompleted}
+        />
+        <PlayerList
+          playerData={this.playerData}
+          toggleCompleted={this.toggleCompleted}
+        />
+      </div>
+      // {/* <div> */}
+      // {/* {playerData.map(player => {
+      //   return(
+      //     <p>{player.name}</p>
+      //     <p>{player.country}</p>
+      //      <p>{player.searches}</p>
+      //   )})
+      // } */}
+      // </div>
+    );
+  }
+}
+
+
 
 export default App;
